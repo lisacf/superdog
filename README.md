90b02b1a6eedbbbc5cf37a7265e1d779676e5720
Lisa Friedman <lisacf@gmail.com>
2013-11-25 14:45:29 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+== README
+
+Fun Dog Portfolio Page.  Focused on responsive design and an awesome skilled dog!
+Ruby on Rails backend.
+Responsive CSS3 HTML5 javascript frontend
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Canine::Application.config.secret_key_base = 'fafce3f79efe45c0e88d5e97ca83d1dbf2ae6af0ebf5b7b32fcaf3f4a77f3b9b9ddbb6a4cdb0b1d3d042127817ffb7211cfe0d4850e4648800e7ec64cb265248'
