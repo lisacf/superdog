62e9c1b4ea86e03029c939171caa2a76435ad135
Lisa Friedman <lisacf@gmail.com>
2013-11-26 00:38:40 -0800

    top banner contact and video section

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,34 +1,29 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
 
-  # GET /contacts
-  # GET /contacts.json
   def index
     @contacts = Contact.all
   end
 
-  # GET /contacts/1
-  # GET /contacts/1.json
   def show
   end
 
-  # GET /contacts/new
   def new
     @contact = Contact.new
   end
 
-  # GET /contacts/1/edit
   def edit
   end
 
-  # POST /contacts
-  # POST /contacts.json
   def create
     @contact = Contact.new(contact_params)
 
     respond_to do |format|
       if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.html do 
+          flash[:notice]='BOW WOW! Thanks for the message.'
+          redirect_to root_path
+        end
         format.json { render action: 'show', status: :created, location: @contact }
       else
         format.html { render action: 'new' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Canine::Application.routes.draw do
   resources :photos
-
   resources :contacts
 
-  root to: "welcome#index"
+  root to: "contacts#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
